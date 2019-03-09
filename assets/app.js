b26838ca1b3c52cc0303160bf2009a89c00b113b
thajedster <jedlawrencecheng@gmail.com>
2019-03-09 13:43:40 -0500

    added framework in html and initialized css file, started pseudocode

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+//display buttons array
+//on click display images
+//on img click animate gif
+//input text add button
+
+var topics = ["cat", "dog", "bird", "skunk", "shark", "turtle", "snake", "rabbit", "horse", "bear", "goat"];
+
+for (i = 0; i < topics.length; i++) {
+
+}
